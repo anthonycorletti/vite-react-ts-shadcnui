270833c0d4b939cdabc594ccc22e2e18163a7e7b
Anthony Corletti <anthcor@gmail.com>
2025-02-04 18:04:14 -0500

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "./lib/utils";
 
 export default function App() {
-    const [count, setCount] = useState(0);
-    return (
-        <ThemeProvider>
-            <div className={cn("flex flex-row items-center justify-center space-x-2")}>
-                <img src="/vite.svg" alt="it's react" />
-                <div className={cn("flex items-center justify-center min-h-screen")}>
-                    <Button onClick={() => setCount(count + 1)}>
-                        Click me {count}
-                    </Button >
-                </div>
-            </div>
-        </ThemeProvider>
-    )
+  const [count, setCount] = useState(0);
+  return (
+    <ThemeProvider>
+      <div
+        className={cn("flex flex-row items-center justify-center space-x-2")}
+      >
+        <img src="/vite.svg" alt="it's react" />
+        <div className={cn("flex items-center justify-center min-h-screen")}>
+          <Button onClick={() => setCount(count + 1)}>Click me {count}</Button>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
